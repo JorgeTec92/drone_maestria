b20d50e44bf56f61f381598b98ffd28112bcfa0a
Jorge <Jorge_andrade1192@hotmail.com>
2024-09-19 13:12:44 -0600

    add rgb vision

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from ultralytics import YOLO
 model = YOLO("weights/roof/best.pt")
 
 "Agregar limite"
-threshold = 0.90
+threshold = 0.92
 
 "Conectar drone"
 tello = Tello()
@@ -24,20 +24,21 @@ def detectar():
 
     while True:
         frame = tello.get_frame_read().frame
-        results = model.predict(frame)[0]
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = model.predict(img)[0]
 
         for result in results.boxes.data.to():
             x1,y1,x2,y2,score,classId = result
 
             if score > threshold:
                 print("Porcentaje", score)
-                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
-                cv2.putText(frame, results.names[int(classId)], (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1.3,(0, 255, 0), 2)
-                cv2.imwrite("captured_photos/"f'photo_{count + 1}.jpg', frame)
+                cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+                cv2.putText(img, results.names[int(classId)], (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1.3,(0, 255, 0), 2)
+                cv2.imwrite("captured_photos/"f'photo_{count + 1}.jpg', img)
                 count += 1
                 cv2.waitKey(5000)
 
-        cv2.imshow("DJI drone", frame)
+        cv2.imshow("DJI drone", img)
 
         "Salir del programa"
         k = cv2.waitKey(30)
